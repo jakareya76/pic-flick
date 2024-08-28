ce5b6020f3689c78faafb13e2a3af23fd672e19e
jakareya76 <jakareya1306@gmail.com>
2024-08-28 16:07:52 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. In doloribus a
+        voluptatum cum magni libero possimus sequi ad maxime cupiditate,
+        corrupti ducimus accusantium odit minima magnam nulla debitis! Fugit,
+        amet?
+      </p>
+    </div>
+  );
+};
+
+export default App;
