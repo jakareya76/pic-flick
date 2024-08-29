c1d82c30fcb6dea651816d95a47abad9a793b696
jakareya76 <jakareya1306@gmail.com>
2024-08-29 21:34:01 +0600

    setup routes and create root and auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import "./index.css";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <p className="text-xl">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. In doloribus a
-        voluptatum cum magni libero possimus sequi ad maxime cupiditate,
-        corrupti ducimus accusantium odit minima magnam nulla debitis! Fugit,
-        amet?
-      </p>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import PrivateRoute from "./Routes/PrivateRoute";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
